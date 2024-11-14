65033961bf90beca1838eaaa69151eb4b515c130
h88ych <hathaitat88@gmail.com>
2024-11-14 22:12:20 +0700

    controller & routes

--- Backend/app/controllers/tasks_controller.ts
+++ Backend/app/controllers/tasks_controller.ts
@@ -2,9 +2,52 @@ import Task from '#models/task'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class TasksController {
-  async index({ res }: HttpContext) {
+  async getAllTask({ response }: HttpContext) {
     const tasks = await Task.query()
-    console.log(tasks)
-    return tasks
+    return response.json(tasks)
+  }
+
+  async createTask({ request }: HttpContext) {
+    try {
+      const req = request.only(['task_name', 'status'])
+      const createdTask = await Task.create(req)
+      return createdTask
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async deleteTask({ response, params }: HttpContext) {
+    const deleteId = params.id
+    console.log(deleteId)
+    try {
+      const findTask = await Task.findByOrFail(deleteId)
+
+      if (!findTask) {
+        return response.status(404).json({ messages: 'This TaskId is not found' })
+      }
+      await findTask.delete()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async updateTask({ params, request, response }: HttpContext) {
+    try {
+      const findTask = await Task.findByOrFail('id', params.id)
+
+      if (!findTask) {
+        return response.status(404).json({ messages: 'This TaskId is not found' })
+      }
+
+      const data = request.only(['task_name', 'status'])
+
+      findTask.merge(data)
+      await findTask.save()
+
+      return findTask
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
--- Backend/start/routes.ts
+++ Backend/start/routes.ts
@@ -10,10 +10,12 @@
 const TasksController = () => import('#controllers/tasks_controller')
 import router from '@adonisjs/core/services/router'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+router.on('/').redirect('/tasks')
 
-router.get('/tasks', [TasksController, 'index']).as('todo.home')
+router.get('/tasks', [TasksController, 'getAllTask']).as('todo.home')
+
+router.post('/tasks', [TasksController, 'createTask']).as('todo.create')
+
+router.delete('/tasks/:id', [TasksController, 'deleteTask']).as('todo.delete')
+
+router.put('/tasks/:id', [TasksController, 'updateTask']).as('todo.update')
